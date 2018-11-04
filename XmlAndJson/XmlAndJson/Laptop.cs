b95a84d165d453a269080d6f0ae1d87045c21fe9
Олжас <40593443+ZhaxylykovOlzhas@users.noreply.github.com>
2018-11-04 22:42:08 +0600

    Add files via upload

---
+++ XmlAndJson/XmlAndJson/Laptop.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlAndJson
+{
+    class Laptop
+    {
+        public string LaptopName { get; set; }
+        public string Version { get; set; }
+        public int ProductYear { get; set; }
+        public string NameCompany { get; set; }
+    }
+}
